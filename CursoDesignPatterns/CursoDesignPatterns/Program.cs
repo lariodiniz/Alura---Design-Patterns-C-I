35c255ed69fc7a057196df895bf951cb024f0150
larioDiniz <lario.diniz@palo.com.br>
2017-10-02 13:49:00 -0300

    Aula 01

---
+++ CursoDesignPatterns/CursoDesignPatterns/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Imposto iss = new ISS();
+            Imposto icms = new ICMS();
+
+
+            Orcamento orcament = new Orcamento(500.0);
+            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+
+            calculador.RealizaCalculo(orcament, icms);
+            calculador.RealizaCalculo(orcament, iss);
+
+
+            Imposto iccc = new ICCC();
+
+            Orcamento orcamento = new Orcamento(1500.00);
+            //calculador.RealizaCalculo(orcament, iccc);
+
+            Console.WriteLine(iccc.Calcula(orcament));*/
+
+            EstrategiaDeInvestimento convervador = new Conservador();
+            Conta conta = new Conta();
+            conta.Deposita(1000);
+
+            RealizadorDeInvestimentos investimento = new RealizadorDeInvestimentos();
+            investimento.Investir(conta, convervador);
+
+
+            Console.ReadKey();
+        }
+    }
+}
